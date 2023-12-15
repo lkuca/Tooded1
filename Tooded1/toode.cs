f4b5aa5ce3c74d6ba0c47f40221b3a54a43ffae8
Sergei <Sergei@SERGEI-PC>
2023-12-15 15:35:28 +0200

    Add project files.

---
+++ Tooded1/toode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tooded
+{
+    public class toode
+    {
+        public int Id { get; set; }
+        public string toodeNimetus { get; set; }
+
+        public int Kogus { get; set; }
+
+        public float Hind { get; set; }
+
+        public string Pilt { get; set; }
+        public IEnumerable<Kategooria> Kategooriad { get;set; }
+    }
+    public class Kategooria
+    {
+        public int Id { get; set; }
+        public string KategooriaNimetus { get; set; }
+        public string Kirjeldus { get; set; }
+    }
+}
